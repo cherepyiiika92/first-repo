d6bef9891b3ffd4846d3d531abe79471a52ed86d
Коньков Владислав Александрович <konkov.va@Kalugaoblgaz.local>
2021-03-29 14:16:49 +0300

    Добавьте файлы проекта.

---
+++ PicCleaner/Core/PicCleaner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace PicCleaner.Core
+{
+    class PicCleaner
+    {
+        private List<Image> _imageList;
+
+        public PicCleaner()
+        {
+            this._imageList = new List<Image>();
+
+        }
+
+
+        }
+
+    }
+}
